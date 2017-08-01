d3923c3dcdbbaabe42d4612862703af6ac493e35
Aksharakeerthi <akshara2223@gmail.com>
2017-08-01 09:34:16 +0530

    Create factnumber.java

---
+++ factnumber.java
@@ -0,0 +1,13 @@
+# factnumber
+import java.io.*;
+import java.util.*;
+public class factnumber{
+public static void main(String args[]){
+int i,fact=1;
+int k=15;
+for(i=1;i<=k;i++){
+fact=fact*i;
+{
+System.out.println("Fact "+K+" is :" +fact);
+}
+}
